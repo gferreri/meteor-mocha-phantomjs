97875aede453c241796df169f70ac8f5cf6ec1f1
Greg Ferreri <gsferrer@us.ibm.com>
2016-12-12 12:50:03 -0500

    make client/server tests synchronous

--- server.js
+++ server.js
@@ -9,46 +9,17 @@ Meteor.startup(function() {
   Meteor.settings.public.CLIENT_TEST_REPORTER = process.env.CLIENT_TEST_REPORTER;
 });
 
-// Since intermingling client and server log lines would be confusing,
-// the idea here is to buffer all client logs until server tests have
-// finished running and then dump the buffer to the screen and continue
-// logging in real time after that if client tests are still running.
-let serverTestsDone = false;
-let clientLines = [];
-function clientLogBuffer(line) {
-  if (serverTestsDone) {
-    // printing and removing the extra new-line character. The first was added by the client log, the second here.
-    console.log(line.replace(/\n$/, ''));
-  } else {
-    clientLines.push(line);
-  }
-}
-
 function printHeader(type) {
   console.log('\n--------------------------------');
   console.log(`----- RUNNING ${type} TESTS -----`);
   console.log('--------------------------------\n');
 }
 
-let callCount = 0;
 let clientFailures = 0;
 let serverFailures = 0;
-function exitIfDone(type, failures) {
-  callCount++;
-  if (type === 'client') {
-    clientFailures = failures;
-  } else {
-    serverFailures = failures;
-    serverTestsDone = true;
-    printHeader('CLIENT');
-    clientLines.forEach((line) => {
-      // printing and removing the extra new-line character. The first was added by the client log, the second here.
-      console.log(line.replace(/\n$/, ''));
-    });
-  }
 
-  if (callCount === 2) {
-    console.log('All client and server tests finished!\n');
+function printSummaryAndExit() {
+  console.log('All client and server tests finished!\n');
     console.log('--------------------------------');
     console.log(`SERVER FAILURES: ${serverFailures}`);
     console.log(`CLIENT FAILURES: ${clientFailures}`);
@@ -60,33 +31,32 @@ function exitIfDone(type, failures) {
         process.exit(0);
       }
     }
-  }
 }
 
 // Before Meteor calls the `start` function, app tests will be parsed and loaded by Mocha
 function start() {
-  // Run the server tests
-  printHeader('SERVER');
-
   // We need to set the reporter when the tests actually run to ensure no conflicts with
   // other test driver packages that may be added to the app but are not actually being
   // used on this run.
   mochaInstance.reporter(reporter);
 
-  mochaInstance.run((failureCount) => {
-    exitIfDone('server', failureCount);
-  });
-
-  // Simultaneously start phantom to run the client tests
+  // Start phantom to run the client tests
+  printHeader('CLIENT');
   startPhantom({
     stdout(data) {
-      clientLogBuffer(data.toString());
+      console.log(data.replace(/\n$/, ''));
     },
     stderr(data) {
-      clientLogBuffer(data.toString());
+      console.log(data.replace(/\n$/, ''));
     },
     done(failureCount) {
-      exitIfDone('client', failureCount);
+      clientFailures = failureCount
+      // Run the server tests
+      printHeader('SERVER');
+      mochaInstance.run((failureCount) => {
+        serverFailures = failureCount
+        printSummaryAndExit()
+      });
     },
   });
 }
